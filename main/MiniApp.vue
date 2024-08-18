b0cec8453fc846471506e7db3456b179500d1b3a
Sen <at437947@gmail.com>
2024-08-19 00:54:31 +0500

    feature

--- main/MiniApp.vue
+++ main/MiniApp.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 import { MainButton, useWebAppPopup } from 'vue-tg';
 
+import { useWebApp } from 'vue-tg';
+
 const { showAlert } = useWebAppPopup();
+const webApp = useWebApp();
 </script>
 
 <template>
+	{{ webApp.initData }}
 	<MainButton text="Open alert" @click="() => showAlert('Hello!')" />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,9 @@
 <template>
-	<div class="index">
-		the Index page
-		{{ webApp.initData }}
-	</div>
+	<div class="index">the Index page</div>
 </template>
 
 <script setup lang="ts">
-import { useWebApp } from 'vue-tg';
-
 defineOptions({ name: 'Index' });
-
-const webApp = useWebApp();
 </script>
 
 <style scoped></style>
