0dae4c28ef09490618d20376fd33fc3e289f490d
Sen <at437947@gmail.com>
2024-08-19 00:28:09 +0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="application">
+		<nuxt-layout>
+			<nuxt-page />
+		</nuxt-layout>
+		<ClientOnly>
+			<MiniApp />
+		</ClientOnly>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import MiniApp from '@main/MiniApp.vue';
+
+defineOptions({ name: 'Application' });
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+import { fileURLToPath } from 'url';
+
+const config = defineNuxtConfig({
+	modules: ['@vueuse/nuxt', 'nuxt-svgo', 'dayjs-nuxt'],
+	css: ['@/assets/styles/index.scss'],
+	devtools: { enabled: true },
+	app: {
+		head: {
+			script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }],
+		},
+	},
+
+	runtimeConfig: {
+		public: {
+			// API_URL: process.env.API_URL,
+		},
+	},
+
+	svgo: {
+		svgo: false,
+		defaultImport: 'component',
+	},
+
+	alias: {
+		'@assets': fileURLToPath(new URL('./assets', import.meta.url)),
+		'@main': fileURLToPath(new URL('./main', import.meta.url)),
+		'@ui': fileURLToPath(new URL('./main/ui', import.meta.url)),
+		'@components': fileURLToPath(new URL('./main/components', import.meta.url)),
+	},
+
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+						@import "@/assets/styles/variables.scss";
+						@import "@/assets/styles/mixins.scss";
+					`,
+				},
+			},
+		},
+	},
+});
+
+export default config;
